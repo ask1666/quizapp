32738795f838cfa765f5d37bece38fdce0bc15fb
Asbjørn <Geeky_assb@live.no>
2019-12-03 19:32:20 +0100

    Deleted useless files

--- app/src/main/java/com/example/quizen/ui/createquestion/CreateQuestionViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.quizen.ui.createquestion;
-
-import androidx.lifecycle.ViewModel;
-
-public class CreateQuestionViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
